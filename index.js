15c25f71ed41c8a850729a6684e37534cf29088b
kent <alias_kentin@hotmail.fr>
2021-02-20 18:09:05 +0100

    app done

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 import Autocomplete from './Autocomplete';
 import usStates from './us-states';
 import './main.css';
@@ -8,6 +9,7 @@ const data = usStates.map(state => ({
   text: state.name,
   value: state.abbreviation
 }));
+
 new Autocomplete(document.getElementById('state'), {
   data,
   onSelect: (stateCode) => {
@@ -17,8 +19,15 @@ new Autocomplete(document.getElementById('state'), {
 
 
 // Github Users
-// new Autocomplete(document.getElementById('gh-user'), {
-//   onSelect: (ghUserId) => {
-//     console.log('selected github user id:', ghUserId);
-//   },
-// });
+new Autocomplete(document.getElementById('gh-user'), {
+  external: {
+    endpoint: "https://api.github.com/search/users",
+    search_var: "q",
+    params: {
+      per_page:10
+    }
+  },
+  onSelect: (ghUserId) => {
+    console.log('selected github user id:', ghUserId);
+  },
+});
